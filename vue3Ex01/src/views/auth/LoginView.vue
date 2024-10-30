8e574669819c05d813c7e1a2e30fc09064962ef8
sazeguro <nepal40@gmail.com>
2024-10-30 18:30:35 +0900

    기본 view 파일 생성

---
+++ vue3Ex01/src/views/auth/LoginView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>login view</div>
+    {{ abc }}
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const abc = ref()
+
+abc.value = 12 * 33;
+
+</script>
+
+<style scoped>
+</style>
